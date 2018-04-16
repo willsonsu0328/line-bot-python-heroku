66e1c5595ef8dea0078318bf138d8243a9edf482
Wilson <wilson@linkwish.com>
2018-04-16 15:15:52 +0800

    modified test log

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def handle_text_message(event):
                   )
 
         line_bot_api.reply_message(event.reply_token, message)
-    p("postbackData:"+event.type)
+    p("postbackData:"+event.postback.data)
 
 def p(log):
   print(log) 
